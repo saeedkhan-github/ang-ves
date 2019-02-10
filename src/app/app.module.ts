7b1a35eaac0a906df767868db32ddf3153cba009
saeedkhan-github <saeedullah321@gmail.com>
2019-02-10 12:24:49 +0500

    uform component removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
-import { UformComponent } from './uform/uform.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UformComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
